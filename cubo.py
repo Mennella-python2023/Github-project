5f27204ca83af7393bf0e5d5f6b8d86f77ae2089
Mennella-python2023 <mennella.maddalena@itisgalileiroma.it>
2024-02-16 09:41:09 +0100

    First commit

---
+++ cubo.py
@@ -0,0 +1,13 @@
+def cubo(c):
+    """
+    Funzione che calcola
+    il cubo
+    del numero passato come parametro
+    """
+    return(c*c*c)
+
+a = float(input("Inserisci il numero: "))
+
+b = cubo(a)
+print(a, "elevato al cubo è uguale a ", b)
+
